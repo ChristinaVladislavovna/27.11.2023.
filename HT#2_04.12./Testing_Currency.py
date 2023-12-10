71d5101f2b8d0590f0ffd00e50e4f05260e27e29
Christina <13popovakris@gmail.com>
2023-12-10 23:48:26 +0100

    07.12.23/.2

--- HT#2_04.12./Testing_Currency.py
+++ HT#2_04.12./Testing_Currency.py
@@ -17,24 +17,24 @@ print(f"| {UAH_EURO_BUY:^10.2f} | {'EUR':^9} | {UAH_EURO_SELL:^9.2f} |")
 print(f"| {UAH_GBP_BUY:^10.2f} | {'GBP':^9} | {UAH_GBP_SELL:^9.2f} |")
 print("**"*19)
 
-def exchange_operation():
-    amount = float(input("Введите сумму для обмена: "))
-    operation = input("Выберите операцию (купить - 'buy' / продать - 'sell'): ")
-
-    if operation == 'buy':
-        # Цены на покупку и продажу, установите сами
-        usd_to_uah = 28.5  # Курс доллара к гривне для покупки
-        result = amount * usd_to_uah
-        print(f"Вы получите {result} гривен за {amount} долларов.")
-    elif operation == 'sell':
-        usd_to_uah = 27.8  # Курс доллара к гривне для продажи
-        result = amount * usd_to_uah
-        print(f"Вы получите {result} долларов за {amount} гривен.")
-    else:
-        print("Выбрана недопустимая операция. Пожалуйста, выберите 'buy' или 'sell'.")
-
-# Вызываем функцию обмена
-exchange_operation()
+# def exchange_operation():
+#     amount = float(input("Введите сумму для обмена: "))
+#     operation = input("Выберите операцию (купить - 'buy' / продать - 'sell'): ")
+#
+#     if operation == 'buy':
+#         # Цены на покупку и продажу, установите сами
+#         usd_to_uah = 28.5  # Курс доллара к гривне для покупки
+#         result = amount * usd_to_uah
+#         print(f"Вы получите {result} гривен за {amount} долларов.")
+#     elif operation == 'sell':
+#         usd_to_uah = 27.8  # Курс доллара к гривне для продажи
+#         result = amount * usd_to_uah
+#         print(f"Вы получите {result} долларов за {amount} гривен.")
+#     else:
+#         print("Выбрана недопустимая операция. Пожалуйста, выберите 'buy' или 'sell'.")
+#
+# # Вызываем функцию обмена
+# exchange_operation()
 
 
 def exchange_operation():
@@ -50,7 +50,7 @@ def exchange_operation():
     elif operation == 'sell':
         amount_usd = float(input("Введите сумму в долларах для продажи за гривны: "))
         result_uah = amount_usd * UAH_USD_SELL
-        print(f"Вы получите {result_uah:.2f} гривен за {amount_usd} долларов.")
+        print(f"Вы получите {result_uah:.2f} гривен за {amount_usd:.2f} долларов.")
     else:
         print("Выбрана недопустимая операция. Пожалуйста, выберите 'buy' или 'sell'.")
 
@@ -58,5 +58,29 @@ def exchange_operation():
 exchange_operation()
 
 
+# currencies = {
+#         'usd': {'buy': 40, 'sell': 42},
+#         'euro': {'buy': 45, 'sell': 47},
+#         'gbp': {'buy': 50, 'sell': 52}
+#     }
+#
+# currency = input("Выберите валюту (usd / euro / gbp): ").lower()
+# operation = input("Выберите операцию (купить - 'buy' / продать - 'sell'): ")
+#
+# if currency in currencies:
+#     buy_rate = currencies[currency]['buy']
+#     sell_rate = currencies[currency]['sell']
+#
+#     if operation == 'buy':
+#         amount_uah = float(input(f"Введите сумму в гривнах для покупки {currency.upper()}: "))
+#         result_currency = amount_uah / buy_rate
+#         print(f"Вы получите {result_currency:.2f} {currency.upper()} за {amount_uah:.2f} гривен.")
+#     elif operation == 'sell':
+#         amount_currency = float(input(f"Введите сумму в {currency.upper()} для продажи за гривны: "))
+#         result_uah = amount_currency * sell_rate
+#         print(f"Вы получите {result_uah:.2f} гривен за {amount_currency:.2f} {currency.upper()}.")
+#     else:
+#         print("Выбрана недопустимая операция. Пожалуйста, выберите 'buy' или 'sell'.")
+# else:
+#     print("Выбрана недопустимая валюта. Пожалуйста, выберите из 'usd', 'euro' или 'gbp'.")
 
-print(f"Ваше количество долларов {result:.2f}, Ваша сдача {remain:.2f} грн")

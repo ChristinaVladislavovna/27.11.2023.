b25c186cd2d351a061142354d30dcdf4d28d4e93
Christina <13popovakris@gmail.com>
2023-12-14 10:32:57 +0100

    11.12.

--- HT#4_11.12./HT_Numbers.py
+++ HT#4_11.12./HT_Numbers.py
@@ -1,11 +1,14 @@
-a = int(input("Введіть число a (1 < a < 200): "))
-b = int(input("Введіть число b (1 < b < 200): "))
 
-if 1 < a < 200 and 1 < b < 200:
-    for divisor in range(2, 7):
-        print(f"Divisible by {divisor}:", end=" ")
+a = int(input("Введите число a (1 < a < 200): "))
+b = int(input("Введите число b (1 < b < 200): "))
+
+if 1 <= a <= 200 and 1 <= b <= 200: # Условие
+    if a > b:
+        a, b = b, a # Равнозначность
+    for divisor in range(2, 7): # Строки - группы делимых чисел на 2, 3, 4, 5, 6
+        print(f"Числа, которые делятся на {divisor}:")
         count = 0
-        for number in range(a, b):
+        for number in range(a, b + 1): # + 1 = каждый последующий
             if number % divisor == 0:
                 print(number, end=", ")
                 count += 1
@@ -13,4 +16,4 @@ if 1 < a < 200 and 1 < b < 200:
                     print()
         print()
 else:
-    print("Числа a та b повинні бути у межах від 1 до 200.")
+    print("Числа a и b должны быть в рамках от 1 до 200.")
